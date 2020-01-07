428ab73e89ff63eae35a2ae1913f84890653dff5
AcerYue0 <leeyuchin2618005@gmail.com>
2020-01-08 05:46:52 +0800

    ordinary setting change
    add getScript() in sidebar loading
    lock and disable scaling and zooming

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
     $("#sidebar").mCustomScrollbar({
         theme: "minimal"
+        
     });
 
     $('#dismiss, .overlay').on('click', function () {
-        $('#sidebar').removeClass('active');
+        $('.sidebarDiv').removeClass('active');
         $('.overlay').removeClass('active');
     });
 
     $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').addClass('active');
+        $('.sidebarDiv').addClass('active');
         $('.overlay').addClass('active');
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
